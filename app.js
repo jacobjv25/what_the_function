7739f44c05c99b53aefd67cdf94a2e0fd6859096
Nolan Raadt <thegodfatherpart2@Nolans-MacBook-Pro.local>
2017-01-06 11:04:32 -0600

    assignment mostly done

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ function Student(firstName, favoriteColor) {
  * that represent you and your coding partner(s).
  */
 
+
+ var nolan = new Student("Nolan", "ocean blue");
+ var jake = new Student("Jake", "blue");
+
+
 /**
   * 2. Create a function that takes one argument. That argument will
   * represent a single student object. The function will log the student's
   * name.
   */
+function user(student) {
+  console.log(student.firstName);
+}
 
 /***** SWITCH PROGRAMMERS *****/
 
@@ -21,10 +29,15 @@ function Student(firstName, favoriteColor) {
   * represent a single student object. The function will log the student's
   * favoriteColor.
   */
+function userColor(student) {
+  console.log(student.favoriteColor);
+}
 
 /**
   * 4. Put both students created in Step 1 into an array called students.
   */
+
+var studentArray = [nolan, jake];
 
 /***** SWITCH PROGRAMMERS *****/
 
@@ -32,3 +45,7 @@ function Student(firstName, favoriteColor) {
  * 5. Loop over the students array and invoke both functions from Step 2 and
  * Step 3. You should see console logs for each students firstName and favoriteColor.
  */
+for (var i = 0; i <studentArray.length; i++) {
+  user(i);
+  userColor(i);
+}
